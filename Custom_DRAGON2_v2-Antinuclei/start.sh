8b5e95869e2f560ae16beb3e39373659956d18b7
Pedro de la Torre Luque <35842752+tospines@users.noreply.github.com>
2023-03-25 12:57:40 +0100

    Delete start.sh

--- Custom_DRAGON2_v2-Antinuclei/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Preparing the system to install..."
-aclocal --force
-libtoolize -c -f
-automake -a -c -f
-autoconf -i -f -v
-echo "Done. Run the configure script to set up the makefiles"
